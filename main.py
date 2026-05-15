ff298aa30e0c495aae9c686dade62ea0f9780e96
ria s <0304RIAS@GMAIL.COM>
2026-05-16 02:00:02 +0530

    add Railway to CORS

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://wya-whats-your-aesthetic.netlify.app",
+        "https://wya-whats-your-aesthetic-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
