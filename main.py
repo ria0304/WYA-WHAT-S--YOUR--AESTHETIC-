b14efe65279cafc54e500256ffa3ade27677f72b
ria s <0304RIAS@GMAIL.COM>
2026-05-16 01:43:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://wya-whats-your-aesthetic.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
